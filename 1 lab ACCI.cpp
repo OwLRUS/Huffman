416fde20a652873fa41d0c828b03002641aac91e
Бриц Владислав <81351838+OwLRUS@users.noreply.github.com>
2022-05-27 20:34:42 +0700

    Add files via upload

---
+++ 1 lab ACCI.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Node
+{
+    int frequence;
+    char letter;
+    Node *leftChild;
+    Node *rightChild;
+public:
+    Node(int freq, char lett);
+    Node() { frequence = 0; letter = -1; }
+
+    int getFrequence() { return frequence; }
+    char getLetter() { return letter; }
+    Node& getLeftChild() { return *leftChild; }
+    Node& getRightChild() { return *rightChild; }
+
+    void addChild(Node*);
+
+    bool isLeaf();
+};
+
+Node::Node(int freq, char lett)
+{
+    frequence = freq;
+    letter = lett;
+    leftChild = NULL;
+    rightChild = NULL;
+}
+
+void Node::addChild(Node *Child)
+{
+    if (!leftChild) leftChild = Child;
+    else
+    {
+        if (leftChild->getFrequence() <= Child->getFrequence())
+            rightChild = Child;
+        else
+        {
+            rightChild = leftChild;
+            leftChild = Child;
+        }
+    }
+
+    frequence += Child->getFrequence();
+}
+
+bool Node::isLeaf()
+{
+    if (!leftChild && !rightChild) return true;
+    return false;
+}
+
+int main()
+{
+    cout << "Hello World!\n";
+}
